7ea40eac2e7dbb5022d0b7b223caa41beba776a5
Duncan Mak <duncan.mak@xamarin.com>
2016-01-26 16:37:15 -0500

    Add typings for d3-shape

---
+++ d3-shape/d3-shape.d.ts
@@ -0,0 +1,34 @@
+declare module 'd3-shape' {
+    export function line<T>(): Line<T>;
+
+    interface Line<T> {
+        (data: T[]): string;
+
+        x(): number | ((d: T, i: number) => number);
+        x(x: number): Line<T>;
+        x(x: (d: T, i: number) => number): Line<T>;
+
+        y(): number | ((d: T, i: number) => number);
+        y(x: number): Line<T>;
+        y(y: (d: T, i: number) => number): Line<T>;
+
+        defined(): (d: T, i: number) => boolean;
+        defined(defined: (d: T, i: number) => boolean): Line<T>;
+
+        // TODO
+        curve();
+
+        context(ctx): this;
+        context(): any;
+
+        radialLine(): RadialLine;
+    }
+
+    interface RadialLine {
+        angle();
+        radius();
+        defined();
+        curve();
+        context();
+    }
+}
